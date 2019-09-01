154eca97a4135accc46ed95630f965876d05d2d4
kampolaczek <kampolaczek@gmail.com>
2019-09-01 21:02:40 +0200

    Login Form Error Handling

--- RMDesktopUI/ViewModels/LoginViewModel.cs
+++ RMDesktopUI/ViewModels/LoginViewModel.cs
@@ -42,6 +42,40 @@ namespace RMDesktopUI.ViewModels
             }
         }
 
+        
+
+        public bool IsErrorVisible
+        {
+            get
+            {
+                bool output = false;
+
+                if (ErrorMessege?.Length > 0)
+                {
+                    output = true;
+                }
+                return output;
+            }
+            
+            
+        }
+
+        private string _errorMessege;
+
+        public string ErrorMessege
+        {
+            get { return _errorMessege; }
+            set
+            {
+                _errorMessege = value;
+                NotifyOfPropertyChange(() => IsErrorVisible);
+                NotifyOfPropertyChange(() => ErrorMessege);
+                
+            }
+        }
+
+
+
         public bool CanLogIn
         {
             get
@@ -59,12 +93,14 @@ namespace RMDesktopUI.ViewModels
         {
             try
             {
+                ErrorMessege = "";
                 var result = await _apiHelper.Authenticate(UserName, Password);
+                
             }
             catch (Exception ex)
             {
 
-                Console.WriteLine(ex.Message);
+                ErrorMessege = ex.Message;
             }
         }
     }
